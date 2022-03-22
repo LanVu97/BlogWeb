93d3f05eaaee3b7a2032fae7c366300328081362
Hoang Lan <lanvth97@gmail.com>
2022-03-22 23:24:18 +0700

    feat: add user model

---
+++ blogWeb/account/admin.py
@@ -0,0 +1,28 @@
+
+
+# Register your models here.
+# src/users/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'username')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'username', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(User, CustomUserAdmin)
