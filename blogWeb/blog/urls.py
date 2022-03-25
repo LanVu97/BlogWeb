c965c75f10599ac477ef48c09707ffb0ffbad802
Hoang Lan <lanvth97@gmail.com>
2022-03-25 16:19:27 +0700

    feat: blog detail

---
+++ blogWeb/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+import blog.views as views
+
+urlpatterns = [
+    path('category/<slug:category_slug>/<slug:blog_slug>', views.blog_detail, name='blog_detail'),
+   
+]
--- blogWeb/blog/views.py
+++ blogWeb/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import  render, redirect
 
-# Create your views here.
+
+from django.contrib import messages
+
+from blog.models import Post
+
+
+
+def blog_detail(request, category_slug, blog_slug):
+    content ={}
+    # get post
+    
+    post = get_object_or_404(Post, slug=blog_slug)
+    # get catagory
+    # get author
+    content['post'] = post
+    return render (request, 'blog/detail.html',content)
