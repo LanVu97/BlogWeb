d793b28e198d775c6374924f822ac60fff8cfd98
Hoang Lan <lanvth97@gmail.com>
2022-03-25 09:59:23 +0700

    feat: login functionality

--- blogWeb/account/urls.py
+++ blogWeb/account/urls.py
@@ -3,5 +3,6 @@ import account.views as views
 
 urlpatterns = [
     path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
 
 ]
--- blogWeb/account/views.py
+++ blogWeb/account/views.py
@@ -20,3 +20,20 @@ def register_request(request):
 		form = UserForm()
 	return render (request, 'account/register.html', {'form': form})
 
+
+def login_request(request):
+	if request.method == "POST":
+		email = request.POST['email']
+		password = request.POST['password']
+		user = authenticate( email=email, password = password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, f' welcome, {user.username} !!!')
+			return render(request,'index.html')
+			
+		else:
+			messages.warning(request, f'account done not exit plz sign in')
+			return redirect('login')
+
+	return render (request, 'account/login.html')
+
